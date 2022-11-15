ea48789883546367e034a6c3d4cadd73b5c5ee8d
JuanIgnacio <JuanIgnacio@LAPTOP-AUBB78NR>
2022-11-15 23:20:11 +0100

    Modificada consola para que sea más correcto (cada clase devolviendo el valor adecuado) su funcionamiento y terminada la clase main y con ello el programa.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,10 +1,73 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Consola;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Direccion;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 
 public class MainApp {
 	
-	public static void main(String[] args) {
+	private static Reina reina;
+	
+	private static void ejecutarOpcion(int opcion) {
+		switch (opcion) {
+			case 0://despedirse
+				Consola.despedirse();
+				break;
+			case 1://reina por defecto
+				reina = crearReinaDefecto();
+				mostrarReina();
+				break;
+			case 2://reina color
+				reina = crearReinaColor(Consola.elegirOpcion());
+				mostrarReina();
+				break;
+			case 3://mover reina
+				mover();
+				mostrarReina();
+				break;
+		}
+	}
+	
+	private static Reina crearReinaDefecto() {
+		return new Reina();
+	}
+	
+	private static Reina crearReinaColor(Color color) {
+		return new Reina(color);
+	}
+	
+	private static void mover() {
+		Direccion direccion;
+		int pasos;
+		
 		Consola.mostrarMenuDirecciones();
+		direccion = Consola.elegirDireccion();
+		pasos = Consola.elegirPasos();
+		try {
+			reina.mover(direccion, pasos);
+		} catch (OperationNotSupportedException | IllegalArgumentException | NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	private static void mostrarReina() {
+		if (reina == null) {
+			System.out.println("La reina no ha sido creada");
+		}else {
+			System.out.println(reina.toString());
+		}
+	}
+	
+	public static void main(String[] args) {
+		int opcion;
+		do {
+		Consola.mostrarMenu();
+		opcion = Consola.elegirOpcionMenu();
+		ejecutarOpcion(opcion);
+		}while(opcion != 0);
 	}
 }
--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
@@ -34,18 +34,28 @@ public class Consola {
 		return opcion;
 	}
 	
-	public static int elegirOpcion() {
+	public static Color elegirOpcion() {
 		int opcion;
+		Color color;
 		
-		System.out.println("Escoge una opcion");
+		System.out.println("Escoge un color");
+		System.out.println();
+		System.out.println("1 - " + Color.BLANCO.toString());
+		System.out.println("2 - " + Color.NEGRO.toString());
 		
 		opcion = Entrada.entero();
 		
-		while(opcion != 1 | opcion != 2) {
+		while(opcion != 1 & opcion != 2) {
 			System.out.println("Opcion no valida, vuelve a escoger.");
 			opcion = Entrada.entero();
 		}
-		return opcion;
+		//Uso color para clarificar el codigo y usar un solo return en vez de 1 por cada color.
+		if(opcion == 1) {
+			color = Color.BLANCO;
+		}else {
+			color =Color.NEGRO;
+		}
+		return color;
 	}
 	
 	public static void mostrarMenuDirecciones () {
@@ -53,14 +63,16 @@ public class Consola {
 		System.out.println();
 		System.out.println("-------------------------");
 		System.out.println();
-		//Esto se podria hacer con 8 syso pero ueria ver si era capaz de hacerlo así
+		//Esto se podria hacer con 8 syso pero queria ver si era capaz de hacerlo así
 		for(int i = 0; i < Direccion.values().length; i++) {
 			System.out.println((i+1) + " - " + Direccion.values()[i].toString().substring(0,1)+Direccion.values()[i].toString().substring(1,(Direccion.values()[i].toString().length())).toLowerCase());
 		}
 	}
 	
-	public static int elegirDireccion() {
+	public static Direccion elegirDireccion() {
 		int opcion;
+		//Inicializo direccion a null para poder hacer el return ya que debe tener valor, aunque nunca deberia devolver null
+		Direccion direccion = null;
 		
 		System.out.println("Escoge una opcion");
 		
@@ -70,7 +82,34 @@ public class Consola {
 			System.out.println("Opcion no valida, vuelve a escoger.");
 			opcion = Entrada.entero();
 		}
-		return opcion;
+		switch (opcion) {
+		//Utilizo Direccion.values()[] para que tenga el mismo valor que en el syso de mostrarMenuDirecciones()
+			case 1:
+				direccion = Direccion.values()[0];
+				break;
+			case 2:
+				direccion = Direccion.values()[1];
+				break;
+			case 3:
+				direccion = Direccion.values()[2];
+				break;
+			case 4:
+				direccion = Direccion.values()[3];
+				break;
+			case 5:
+				direccion = Direccion.values()[4];
+				break;
+			case 6:
+				direccion = Direccion.values()[5];
+				break;
+			case 7:
+				direccion = Direccion.values()[6];
+				break;
+			case 8:
+				direccion = Direccion.values()[7];
+				break;
+		}
+		return direccion;
 	}
 	
 	public static int elegirPasos() {
