474397ec82476461f9078a2a9bac6cb1d1e40e7e
JuanIgnacio <JuanIgnacio@LAPTOP-AUBB78NR>
2022-11-15 11:41:26 +0100

    Completada la clase Posicion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,21 +1,36 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
 
 	private int fila;
 	private char columna;
 	
 	private static final int MAX_FILA = 8;
-	private static final int MIN_FILA = 0;
+	private static final int MIN_FILA = 1;
 	private static final char MAX_COLUMNA = 'h';
 	private static final char MIN_COLUMNA = 'a';
+	
+	public Posicion (int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+	}
+	
+	public Posicion (Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
+		}
+		setFila(posicion.getFila());
+		setColumna(posicion.getColumna());
+	}
 	
 	public int getFila() {
 		return fila;
 	}
 	private void setFila(int fila) {
 		if (fila < MIN_FILA | fila > MAX_FILA) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
 		}
 		this.fila = fila;
 	}
@@ -23,10 +38,32 @@ public class Posicion {
 		return columna;
 	}
 	private void setColumna(char columna) {
-		//TODO error
+		if (columna < MIN_COLUMNA | columna > MAX_COLUMNA) {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
 		this.columna = columna;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+
+	@Override
+	public String toString() {
+		return "fila=" + fila + ", columna=" + columna;
+	}
 	
 }
